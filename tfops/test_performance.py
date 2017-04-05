33afdbf407215e9a5078e3379d87a21a91322c04
Andrzej Pronobis <a.pronobis@gmail.com>
2017-04-04 23:11:43 -0700

    Added test cases for all optimization levels

--- tfops/test_performance.py
+++ tfops/test_performance.py
@@ -79,6 +79,35 @@ class TestGatherColumnsPerformance(tf.test.TestCase):
             list(range(self.num_cols - 1, -1, -1)),  # indices: [n-1, n-2, n-3, ..., 1, 0]
             device_name=device_name)
 
+    def run_test_opt10(self, fun, device_name):
+        """Case: In-op optimization 10%"""
+        shuffled_ind = [0, 1, 9, 8, 7, 6, 5, 4, 3, 2]
+        ind = np.empty((self.num_cols // 10, 10))
+        ind[:] = shuffled_ind
+        self.run_test(fun, ind.ravel(), device_name=device_name)
+
+    def run_test_opt40(self, fun, device_name):
+        """Case: In-op optimization 40%"""
+        shuffled_ind = [0, 1, 2, 9, 5, 4, 6, 7, 8, 3]
+        ind = np.empty((self.num_cols // 10, 10))
+        ind[:] = shuffled_ind
+        self.run_test(fun, ind.ravel(), device_name=device_name)
+
+    def run_test_opt70(self, fun, device_name):
+        """Case: In-op optimization 70%"""
+        shuffled_ind = [9, 1, 2, 3, 4, 5, 6, 7, 8, 0]
+        ind = np.empty((self.num_cols // 10, 10))
+        ind[:] = shuffled_ind
+        self.run_test(fun, ind.ravel(), device_name=device_name)
+
+    def run_test_opt100(self, fun, device_name):
+        """Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test(
+            fun,
+            list(range(0, self.num_cols)),  # indices: [0, 1, 2, ..., n-2, n-1]
+            device_name=device_name)
+
     def run_test_tfindexing_opt0(self, device_name):
         """Method: TF Indexing
            Case: Worst-case (in-op optimization not used (0%))"""
@@ -140,6 +169,254 @@ class TestGatherColumnsPerformance(tf.test.TestCase):
            Case: Worst-case (in-op optimization not used (0%))"""
 
         self.run_test_custom_opt0('/gpu:0')
+
+    def run_test_tfindexing_opt10(self, device_name):
+        """Method: TF Indexing
+           Case: In-op optimization 10%"""
+
+        self.run_test_opt10(fun=fun_tfindexing, device_name=device_name)
+
+    def test_tfindexing_cpu_opt10(self):
+        """Method: TF Indexing
+           Device: CPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_tfindexing_opt10('/cpu:0')
+
+    def test_tfindexing_gpu_opt10(self):
+        """Method: TF Indexing
+           Device: GPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_tfindexing_opt10('/gpu:0')
+
+    def run_test_gathernd_opt10(self, device_name):
+        """Method: gather_nd and transpose
+           Case: In-op optimization 10%"""
+
+        self.run_test_opt10(fun=lambda p, i: spn.utils.gather_cols(p, i, use_gather_nd=True),
+                            device_name=device_name)
+
+    def test_gathernd_cpu_opt10(self):
+        """Method: gather_nd and transpose
+           Device: CPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_gathernd_opt10('/cpu:0')
+
+    def test_gathernd_gpu_opt10(self):
+        """Method: gather_nd and transpose
+           Device: GPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_gathernd_opt10('/gpu:0')
+
+    def run_test_custom_opt10(self, device_name):
+        """Method: custom gather_cols op
+           Case: In-op optimization 10%"""
+
+        self.run_test_opt10(fun=fun_custom,
+                            device_name=device_name)
+
+    def test_custom_cpu_opt10(self):
+        """Method: custom gather_cols op
+           Device: CPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_custom_opt10('/cpu:0')
+
+    def test_custom_gpu_opt10(self):
+        """Method: custom gather_cols op
+           Device: GPU
+           Case: In-op optimization 10%"""
+
+        self.run_test_custom_opt10('/gpu:0')
+
+    def run_test_tfindexing_opt40(self, device_name):
+        """Method: TF Indexing
+           Case: In-op optimization 40%"""
+
+        self.run_test_opt40(fun=fun_tfindexing, device_name=device_name)
+
+    def test_tfindexing_cpu_opt40(self):
+        """Method: TF Indexing
+           Device: CPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_tfindexing_opt40('/cpu:0')
+
+    def test_tfindexing_gpu_opt40(self):
+        """Method: TF Indexing
+           Device: GPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_tfindexing_opt40('/gpu:0')
+
+    def run_test_gathernd_opt40(self, device_name):
+        """Method: gather_nd and transpose
+           Case: In-op optimization 40%"""
+
+        self.run_test_opt40(fun=lambda p, i: spn.utils.gather_cols(p, i, use_gather_nd=True),
+                            device_name=device_name)
+
+    def test_gathernd_cpu_opt40(self):
+        """Method: gather_nd and transpose
+           Device: CPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_gathernd_opt40('/cpu:0')
+
+    def test_gathernd_gpu_opt40(self):
+        """Method: gather_nd and transpose
+           Device: GPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_gathernd_opt40('/gpu:0')
+
+    def run_test_custom_opt40(self, device_name):
+        """Method: custom gather_cols op
+           Case: In-op optimization 40%"""
+
+        self.run_test_opt40(fun=fun_custom,
+                            device_name=device_name)
+
+    def test_custom_cpu_opt40(self):
+        """Method: custom gather_cols op
+           Device: CPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_custom_opt40('/cpu:0')
+
+    def test_custom_gpu_opt40(self):
+        """Method: custom gather_cols op
+           Device: GPU
+           Case: In-op optimization 40%"""
+
+        self.run_test_custom_opt40('/gpu:0')
+
+    def run_test_tfindexing_opt70(self, device_name):
+        """Method: TF Indexing
+           Case: In-op optimization 70%"""
+
+        self.run_test_opt70(fun=fun_tfindexing, device_name=device_name)
+
+    def test_tfindexing_cpu_opt70(self):
+        """Method: TF Indexing
+           Device: CPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_tfindexing_opt70('/cpu:0')
+
+    def test_tfindexing_gpu_opt70(self):
+        """Method: TF Indexing
+           Device: GPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_tfindexing_opt70('/gpu:0')
+
+    def run_test_gathernd_opt70(self, device_name):
+        """Method: gather_nd and transpose
+           Case: In-op optimization 70%"""
+
+        self.run_test_opt70(fun=lambda p, i: spn.utils.gather_cols(p, i, use_gather_nd=True),
+                            device_name=device_name)
+
+    def test_gathernd_cpu_opt70(self):
+        """Method: gather_nd and transpose
+           Device: CPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_gathernd_opt70('/cpu:0')
+
+    def test_gathernd_gpu_opt70(self):
+        """Method: gather_nd and transpose
+           Device: GPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_gathernd_opt70('/gpu:0')
+
+    def run_test_custom_opt70(self, device_name):
+        """Method: custom gather_cols op
+           Case: In-op optimization 70%"""
+
+        self.run_test_opt70(fun=fun_custom,
+                            device_name=device_name)
+
+    def test_custom_cpu_opt70(self):
+        """Method: custom gather_cols op
+           Device: CPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_custom_opt70('/cpu:0')
+
+    def test_custom_gpu_opt70(self):
+        """Method: custom gather_cols op
+           Device: GPU
+           Case: In-op optimization 70%"""
+
+        self.run_test_custom_opt70('/gpu:0')
+
+    def run_test_tfindexing_opt100(self, device_name):
+        """Method: TF Indexing
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_opt100(fun=fun_tfindexing, device_name=device_name)
+
+    def test_tfindexing_cpu_opt100(self):
+        """Method: TF Indexing
+           Device: CPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_tfindexing_opt100('/cpu:0')
+
+    def test_tfindexing_gpu_opt100(self):
+        """Method: TF Indexing
+           Device: GPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_tfindexing_opt100('/gpu:0')
+
+    def run_test_gathernd_opt100(self, device_name):
+        """Method: gather_nd and transpose
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_opt100(fun=lambda p, i: spn.utils.gather_cols(p, i, use_gather_nd=True),
+                             device_name=device_name)
+
+    def test_gathernd_cpu_opt100(self):
+        """Method: gather_nd and transpose
+           Device: CPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_gathernd_opt100('/cpu:0')
+
+    def test_gathernd_gpu_opt100(self):
+        """Method: gather_nd and transpose
+           Device: GPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_gathernd_opt100('/gpu:0')
+
+    def run_test_custom_opt100(self, device_name):
+        """Method: custom gather_cols op
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_opt100(fun=fun_custom,
+                             device_name=device_name)
+
+    def test_custom_cpu_opt100(self):
+        """Method: custom gather_cols op
+           Device: CPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_custom_opt100('/cpu:0')
+
+    def test_custom_gpu_opt100(self):
+        """Method: custom gather_cols op
+           Device: GPU
+           Case: Best-case (in-op optimization 100%)"""
+
+        self.run_test_custom_opt100('/gpu:0')
 
 
 if __name__ == '__main__':
