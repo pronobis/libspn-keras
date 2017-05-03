1c5588a6c19b13c0e5fed35c850c2979ad20ffdf
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-02 22:33:07 -0700

    More options for performance testing

--- libspn/tests/perf_gather_cols.py
+++ libspn/tests/perf_gather_cols.py
@@ -60,16 +60,20 @@ class Ops:
 
 
 class OpTestResult:
+    """Result of a single test of a single op."""
 
-    def __init__(self, op_name, on_gpu, graph_size, setup_time, run_times):
+    def __init__(self, op_name, on_gpu, graph_size, setup_time,
+                 run_times, output_correct):
         self.op_name = op_name
         self.on_gpu = on_gpu
         self.graph_size = graph_size
         self.setup_time = setup_time
         self.run_times = run_times
+        self.output_correct = output_correct
 
 
 class TestResults:
+    """Results for a single test for multiple ops and devices."""
 
     def __init__(self, test_name, cpu_results, gpu_results):
         self.test_name = test_name
@@ -78,16 +82,17 @@ class TestResults:
 
     def print(self):
         def print_header(dev):
-            print1("%3s %11s: %5s %11s %15s %14s" %
+            print1("%3s %11s: %5s %11s %15s %14s %10s" %
                    (dev, 'op', 'size', 'setup_time',
-                    'first_run_time', 'rest_run_time'))
+                    'first_run_time', 'rest_run_time', 'correct'))
 
         def print_res(res):
             """Helper function printing a single result."""
-            print1("%15s: %5d %11.2f %15.2f %14.2f" %
+            print1("%15s: %5d %11.2f %15.2f %14.2f %10s" %
                    (res.op_name, res.graph_size,
                     res.setup_time * 1000, res.run_times[0] * 1000,
-                    np.mean(res.run_times[1:]) * 1000))
+                    np.mean(res.run_times[1:]) * 1000,
+                    res.output_correct))
 
         # Print results
         print()
@@ -105,13 +110,17 @@ class TestResults:
 class PerformanceTest:
 
     def __init__(self, num_param_rows, num_param_cols, num_indices,
-                 num_ops, num_runs, dtype, log_devs):
+                 num_ops, num_runs, dtype, with_indexing,
+                 with_cpu, with_gpu, log_devs):
         self.num_param_rows = num_param_rows
         self.num_param_cols = num_param_cols
         self.num_indices = num_indices
         self.num_ops = num_ops
         self.num_runs = num_runs
         self.dtype = dtype
+        self.with_indexing = with_indexing
+        self.with_cpu = with_cpu
+        self.with_gpu = with_gpu
         self.log_devs = log_devs
 
         print1("Params:")
@@ -152,6 +161,7 @@ class PerformanceTest:
         # Get num of graph ops
         graph_size = len(tf.get_default_graph().get_operations())
         # Run multiple times
+        output_correct = True
         with tf.Session(config=tf.ConfigProto(
                 allow_soft_placement=False,
                 log_device_placement=self.log_devs)) as sess:
@@ -163,22 +173,30 @@ class PerformanceTest:
                 run_times.append(time.time() - start_time)
                 # Check output
                 for o in out:
-                    np.testing.assert_array_almost_equal(o, true_out)
+                    try:
+                        np.testing.assert_array_almost_equal(o, true_out)
+                    except AssertionError:
+                        output_correct = False
         # Return stats
-        return OpTestResult(op_name, on_gpu, graph_size, setup_time, run_times)
+        return OpTestResult(op_name, on_gpu, graph_size, setup_time,
+                            run_times, output_correct)
 
     def _run_test(self, test_name, op_funs, params, indices):
         """Run a single test for multiple ops and devices."""
         cpu_results = []
         gpu_results = []
         for op_fun in op_funs:
-            cpu_results.append(
-                self._run_op_test(op_fun, params, indices, on_gpu=False))
-            gpu_results.append(
-                self._run_op_test(op_fun, params, indices, on_gpu=True))
+            if self.with_cpu:
+                cpu_results.append(
+                    self._run_op_test(op_fun, params, indices, on_gpu=False))
+            if self.with_gpu:
+                gpu_results.append(
+                    self._run_op_test(op_fun, params, indices, on_gpu=True))
         return TestResults(test_name, cpu_results, gpu_results)
 
     def _run_1d(self):
+        """Run all 1D tests."""
+
         results = []
         params = np.random.rand(self.num_param_cols)
 
@@ -225,6 +243,8 @@ class PerformanceTest:
         return results
 
     def _run_2d(self):
+        """Run all 2D tests."""
+
         results = []
         params = np.random.rand(self.num_param_rows, self.num_param_cols)
 
@@ -249,14 +269,16 @@ class PerformanceTest:
         indices[:] = shuffled_ind
         indices = indices.ravel()
         r = self._run_test('2d_opt_%sindices' % self.num_param_cols,
-                           [Ops.custom, Ops.gather_nd, Ops.indexing],
+                           [Ops.custom, Ops.gather_nd] +
+                           ([Ops.indexing] if self.with_indexing else []),
                            params, indices)
         results.append(r)
 
         # Worst case
         indices = range(self.num_param_cols - 1, -1, -1)
         r = self._run_test('2d_worst_%sindices' % self.num_param_cols,
-                           [Ops.custom, Ops.gather_nd, Ops.indexing],
+                           [Ops.custom, Ops.gather_nd] +
+                           ([Ops.indexing] if self.with_indexing else []),
                            params, indices)
         results.append(r)
 
@@ -264,13 +286,16 @@ class PerformanceTest:
         indices = np.random.randint(low=0, high=self.num_param_cols,
                                     size=self.num_indices)
         r = self._run_test('2d_random_%dindices' % self.num_indices,
-                           [Ops.custom, Ops.gather_nd, Ops.indexing],
+                           [Ops.custom, Ops.gather_nd] +
+                           ([Ops.indexing] if self.with_indexing else []),
                            params, indices)
         results.append(r)
 
         return results
 
     def run(self):
+        """Run all tests."""
+
         results = []
         results += self._run_1d()
         results += self._run_2d()
@@ -288,12 +313,17 @@ def main():
     parser.add_argument('--num_ops', default=50, type=int)
     parser.add_argument('--num_runs', default=10, type=int)
     parser.add_argument('--log-devices', default=False, type=bool)
+    parser.add_argument('--with-indexing', default=False, type=bool)
+    parser.add_argument('--with-cpu', default=True, type=bool)
+    parser.add_argument('--with-gpu', default=True, type=bool)
     dtype = tf.float32
     args = parser.parse_args()
 
     t = PerformanceTest(args.num_param_rows, args.num_param_cols,
                         args.num_indices, args.num_ops, args.num_runs,
-                        dtype, args.log_devices)
+                        dtype, args.with_indexing,
+                        args.with_cpu, args.with_gpu,
+                        args.log_devices)
     t.run()
 
 
