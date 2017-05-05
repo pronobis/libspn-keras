f09fa8d6799193db7cd9fa14849a86ff7776ff9a
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-04 21:09:52 -0700

    Setup.py now only rebuilds when necessary

--- setup.py
+++ setup.py
@@ -83,12 +83,10 @@ class BuildCommand(distutils.command.build.build):
             print("  TF gcc version < system gcc version: "
                   "using -D_GLIBCXX_USE_CXX11_ABI=0")
 
-    def _run_nvcc(self, filename):
-        obj_file = os.path.join(self._build_dir, filename + ".o")
-        src_file = os.path.join(self._src_dir, filename)
+    def _run_nvcc(self, obj, source):
         try:
             cmd = ([self._cuda_nvcc, "-c", "-o",
-                    obj_file, src_file,
+                    obj, source,
                     '-std=c++11', '-x=cu', '-Xcompiler', '-fPIC',
                     '-DGOOGLE_CUDA=1',
                     '--expt-relaxed-constexpr',  # To silence harmless warnings
@@ -102,12 +100,10 @@ class BuildCommand(distutils.command.build.build):
             subprocess.check_call(cmd)  # Used instead of run for 3.4 compatibility
         except subprocess.CalledProcessError:
             os.sys.exit('ERROR: Build error!')
-        return obj_file
 
-    def _run_gcc(self, inputs):
-        lib_file = os.path.join(self._src_dir, "libspn_ops.so")
+    def _run_gcc(self, target, inputs):
         try:
-            cmd = (['g++', '-shared', '-o', lib_file] +
+            cmd = (['g++', '-shared', '-o', target] +
                    inputs +
                    ['-std=c++11', '-fPIC', '-lcudart',
                     '-DGOOGLE_CUDA=1',
@@ -123,21 +119,51 @@ class BuildCommand(distutils.command.build.build):
             subprocess.check_call(cmd)  # Used instead of run for 3.4 compatibility
         except subprocess.CalledProcessError:
             os.sys.exit('ERROR: Build error!')
-        return lib_file
+
+    def _is_dirty(self, target, sources):
+        """Verify if changes have been made to sources and target must
+        be re-built."""
+        t_date = os.path.getmtime(target) if os.path.exists(target) else 0
+        s_date = max(os.path.getmtime(s) for s in sources)
+        return t_date <= s_date
 
     def _build(self):
         print(self._col_head + "Building:" + self._col_clear)
+        # Make paths
         self._build_dir = os.path.abspath(os.path.join('build', 'ops'))
         self._src_dir = os.path.abspath(os.path.join('libspn', 'ops'))
         os.makedirs(self._build_dir, exist_ok=True)
-        gather_obj = self._run_nvcc('gather_columns_functor_gpu.cu.cc')
-        scatter_obj = self._run_nvcc('scatter_columns_functor_gpu.cu.cc')
-        self._run_gcc([gather_obj, scatter_obj] +
-                      [os.path.join(self._src_dir, i) for i in
-                          ['gather_columns.cc',
-                           'gather_columns_functor.cc',
-                           'scatter_columns.cc',
-                           'scatter_columns_functor.cc']])
+        # Define sources and target
+        sources_cuda = [os.path.join(self._src_dir, i) for i in
+                        ['gather_columns_functor_gpu.cu.cc',
+                         'scatter_columns_functor_gpu.cu.cc']]
+        headers_cuda = [os.path.join(self._src_dir, i) for i in
+                        ['gather_columns_functor_gpu.cu.h',
+                         'scatter_columns_functor_gpu.cu.h']]
+        sources = [os.path.join(self._src_dir, i) for i in
+                   ['gather_columns.cc',
+                    'gather_columns_functor.cc',
+                    'scatter_columns.cc',
+                    'scatter_columns_functor.cc']]
+        headers = [os.path.join(self._src_dir, i) for i in
+                   ['gather_columns_functor.h',
+                    'scatter_columns_functor.h']]
+        target = os.path.join(self._src_dir, "libspn_ops.so")
+        # Should rebuild?
+        if self._is_dirty(target, sources + headers +
+                          sources_cuda + headers_cuda):
+            # Compile cuda
+            objects_cuda = []
+            for s, h in zip(sources_cuda, headers_cuda):
+                obj = os.path.join(self._build_dir,
+                                   os.path.basename(s) + ".o")
+                objects_cuda.append(obj)
+                if self._is_dirty(obj, [s, h]):
+                    self._run_nvcc(obj, s)
+            # Compile rest and link
+            self._run_gcc(target, objects_cuda + sources)
+        else:
+            print("Everything up to date.")
 
     def _test(self):
         print(self._col_head + "Testing:" + self._col_clear)
