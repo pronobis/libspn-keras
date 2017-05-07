606a792ea57dbe7b9719a31f84276e68eacfcb18
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-06 22:45:11 -0700

    Updated copyright note

--- libspn/tests/test_nodes_product.py
+++ libspn/tests/test_nodes_product.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # ------------------------------------------------------------------------
-# Copyright (C) 2016 Andrzej Pronobis - All Rights Reserved
+# Copyright (C) 2016-2017 Andrzej Pronobis - All Rights Reserved
 #
 # This file is part of LibSPN. Unauthorized use or copying of this file,
 # via any medium is strictly prohibited. Proprietary and confidential.
