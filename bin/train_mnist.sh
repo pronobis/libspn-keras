50f8fff0b9b93901920363b10a2641a1673afce6
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-12 14:31:15 -0700

    Reworked model

--- bin/train_mnist.sh
+++ bin/train_mnist.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-./spn-model -v  train dense mnist "~/Data/mnist"
+./spn-model -v train dense mnist "~/Data/mnist" \
+            --num-epochs 100 \
+            --batch-size 100 \
+            --shuffle \
+            --seed 100 \
+            --image-format binary \
+            --ratio 2 \
+            --crop 2 \
+            --mnist-subset=train\
+            --dense-input-dist raw \
+            --dense-decomps 1 \
+            --dense-subsets 2 \
+            --dense-mixtures 4 \
+            --dense-input-mixtures 2 \
+            --weight-init random \
+            --value-inference marginal \
+            --init-accum 20 \
+            --smoothing-val 0.0 \
+            --smoothing-decay 0.2 \
+            --smoothing-min 0.0 \
+            --stop-condition 0.05
--- libspn/models/dense.py
+++ libspn/models/dense.py
@@ -11,28 +11,31 @@ from libspn.graph.ivs import IVs
 from libspn.generation.dense import DenseSPNGenerator
 from libspn.utils.math import ValueType
 from libspn.generation.weights import generate_weights
+from libspn import conf
+from libspn.learning.em import EMLearning
+from libspn.graph.weights import initialize_weights
+from libspn.inference.type import InferenceType
+import tensorflow as tf
+from enum import Enum
 
 
 class DenseModel(Model):
 
-    logger = get_logger()
-    info = logger.info
-    debug1 = logger.debug1
-    is_debug1 = logger.is_debug1
+    __logger = get_logger()
+    __info = __logger.info
+    __debug1 = __logger.debug1
+    __is_debug1 = __logger.is_debug1
 
-    def __init__(self, num_decomps, num_subsets, num_mixtures,
-                 input_dist=DenseSPNGenerator.InputDist.MIXTURE,
-                 num_input_mixtures=None):
+    def __init__(self):
         super().__init__()
-        self._num_decomps = num_decomps
-        self._num_subsets = num_subsets
-        self._num_mixtures = num_mixtures
-        self._input_dist = input_dist
-        self._num_input_mixtures = num_input_mixtures
         self._ivs = None
+        self._root = None
 
-    def build_spn(self, dataset):
-        self.info("Building SPN")
+    def build_spn(self, dataset, num_decomps, num_subsets, num_mixtures,
+                  input_dist=DenseSPNGenerator.InputDist.MIXTURE,
+                  num_input_mixtures=None,
+                  weight_init_value=ValueType.RANDOM_UNIFORM(0, 1)):
+        self.__info("Building SPN")
 
         # Get data from dataset
         data = dataset.get_data()
@@ -49,26 +52,52 @@ class DenseModel(Model):
         self._ivs = IVs(num_vars=num_vars, num_vals=num_vals)
 
         # Generate structure
-        dense_gen = DenseSPNGenerator(num_decomps=self._num_decomps,
-                                      num_subsets=self._num_subsets,
-                                      num_mixtures=self._num_mixtures,
-                                      input_dist=self._input_dist,
-                                      num_input_mixtures=self._num_input_mixtures,
+        dense_gen = DenseSPNGenerator(num_decomps=num_decomps,
+                                      num_subsets=num_subsets,
+                                      num_mixtures=num_mixtures,
+                                      input_dist=input_dist,
+                                      num_input_mixtures=num_input_mixtures,
                                       balanced=True)
         self._root = dense_gen.generate(self._ivs)
-        if self.is_debug1():
-            self.debug1("SPN graph with %d nodes" % self._root.get_num_nodes())
+        if self.__is_debug1():
+            self.__debug1("SPN graph with %d nodes" % self._root.get_num_nodes())
 
         # Generate weights
-        self.debug1("Generating weights")
-        weight_init_value = ValueType.RANDOM_UNIFORM(0, 1)
+        self.__debug1("Generating weights")
         generate_weights(self._root, init_value=weight_init_value)
-        if self.is_debug1():
-            self.debug1("SPN graph with %d nodes and %d TF ops" % (
+        if self.__is_debug1():
+            self.__debug1("SPN graph with %d nodes and %d TF ops" % (
                 self._root.get_num_nodes(), self._root.get_tf_graph_size()))
 
-    def train(self):
-        pass
+    def train(self, value_inference_type=InferenceType.MARGINAL,
+              init_accum_value=20, additive_smoothing_value=0.0,
+              additive_smoothing_decay=0.2, additive_smoothing_min=0.0,
+              stop_condition=0.0):
+        self.__info("Adding EM learning ops")
+        additive_smoothing_var = tf.Variable(additive_smoothing_value,
+                                             dtype=conf.dtype)
+        em_learning = EMLearning(
+            self._root, log=True,
+            value_inference_type=value_inference_type,
+            additive_smoothing=additive_smoothing_var,
+            add_random=None,
+            initial_accum_value=init_accum_value,
+            use_unweighted=True)
+        reset_accumulators = em_learning.reset_accumulators()
+        accumulate_updates = em_learning.accumulate_updates()
+        update_spn = em_learning.update_spn()
+        train_likelihood = em_learning.value.values[self._root]
+        avg_train_likelihood = tf.reduce_mean(train_likelihood)
+        self.__info("Adding weight initialization ops")
+        init_weights = initialize_weights(self._root)
+
+        self.__info("Learning...")
+
+    def save_graph(self):
+        self.__info("Saving TensorFlow graph")
+        writer = tf.summary.FileWriter("./logs",
+                                       self._root.tf_graph)
+        writer.flush()
 
     def test(self):
         pass
