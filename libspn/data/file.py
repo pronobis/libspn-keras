14a32232bdf736549b5b52c05d648d36862d89de
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-25 11:46:22 -0700

    New type of file dataset

--- libspn/data/file.py
+++ libspn/data/file.py
@@ -9,18 +9,23 @@ import tensorflow as tf
 from libspn.data.dataset import Dataset
 from libspn import utils
 import os
+import glob
+import fnmatch
+import re
 
 
 class FileDataset(Dataset):
-    """An abstract class for a dataset stored in a file.
+    """
+    An abstract class for a dataset stored in a file.
 
     Args:
-        files (str or list): A string containing a path to a file or a glob
-                             matching multiple files, or a list of paths
-                             to multiple files. Note that the order of files,
-                             when using a glob is not predictable (even with
-                             ``shuffle`` set to ``False``), but will be constant
-                             across epochs.
+        files (str or list of str): A string containing a path to a file or a
+              glob matching multiple files, or a list of paths to multiple
+              files. When glob is used, the files will be sorted, unless
+              ``shuffle`` is set to ``True``. If a part of a path is wrapped in
+              curly braces, it will be extracted as a label for the file. This
+              works even for a glob, e.g. ``dir/{*}.jpg`` will use the filename
+              without the extension as the label.
         num_epochs (int): Number of epochs of produced data.
         batch_size (int): Size of a single batch.
         shuffle (bool): Shuffle data within each epoch.
@@ -55,41 +60,114 @@ class FileDataset(Dataset):
                          seed=seed)
         self.__files = files
 
-    def _get_file_queue(self):
-        """Build a queue serving file names for multiple epochs.
+    def _get_file_label_tensor(self):
+        """
+        Serve filenames and labels for multiple epochs as a pair of tensors.
+        Internally, this is implemented using a single queue, from which both a
+        filename and a label are dequeued. The filename can be fed to a function
+        that reads the file, but it cannot be used with a `Reader` which
+        requires a queue.
 
         Returns:
-            A queue serving file names.
+            tuple: A 2-element tuple containing data taken from the queue:
+
+            - filename (Tensor): A tensor containing the filename.
+            - label (Tensor): A tensor containing the label.
         """
-        if isinstance(self.__files, str):
-            fnames = tf.train.match_filenames_once(
-                os.path.expanduser(self.__files))
-        else:
-            fnames = self.__files
+        files, labels = FileDataset._get_files_labels(self.__files)
+        # Since files is a variable holding all the files, and since
+        # input_producer shuffles all input data before passing it to
+        # the queue, all files will always be shuffled independently
+        # of the capacity parameter of string_input_producer
+        return tf.train.slice_input_producer([files, labels],
+                                             num_epochs=self._num_epochs,
+                                             shuffle=self._shuffle,
+                                             seed=self._seed)
+
+    def _get_file_queue(self):
+        """
+        Build a queue serving file names for multiple epochs. This method can be
+        used in combination with a `Reader` to read multiple data samples from
+        each file. In such case, the labels are assumed to be stored inside the
+        file and extracting them is the responsibility of the reader.
+
+        Returns:
+            FIFOQueue: A queue serving file names.
+        """
+        files, _ = FileDataset._get_files_labels(self.__files)
         # Since fnames is a variable holding all the files, and since
         # input_producer shuffles all input data before passing it to
         # the queue, all files will always be shuffled independently
         # of the capacity parameter of string_input_producer
-        return tf.train.string_input_producer(fnames,
+        return tf.train.string_input_producer(files,
                                               num_epochs=self._num_epochs,
                                               shuffle=self._shuffle,
                                               seed=self._seed)
 
+    @staticmethod
+    def get_files_labels(files):
+        """
+        Convert the file specification to a list of files and labels. The files
+        can be specified using a string containing a path to a file or a glob
+        matching multiple files, or a list of paths to multiple files. When glob
+        is used, the files will be returned sorted. If a part of a path is
+        wrapped in curly braces, it will be extracted as a label for the file.
+        This works even for a glob, e.g. ``dir/{*}.jpg`` will use the filename
+        without the extension as the label.
+
+        Args:
+            files (str or list of str): File specification.
+
+        Returns:
+            tuple: A 2-element tuple containing:
+
+            - files (list): A list of file paths matching the specification.
+            - labels (list): A list of labels discovered for the files. If
+              label has not been indicated for a file, the list contains an
+              empty string for that file.
+        """
+        # Convert single path to a list
+        if isinstance(files, str):
+            files = [files]
+        all_files = []
+        all_labels = []
+        for f in files:
+            # Get files matching a glob
+            f_clean = f.replace('{', '').replace('}', '')
+            f_files = sorted(glob.glob(os.path.expanduser(f_clean)))
+            # Get regexp for extracting labels
+            f_re = fnmatch.translate(f)
+            f_re = f_re.replace('\{', '(?P<label>').replace('\}', ')')
+            f_re = re.compile(f_re)
+            # Extract labels
+            f_labels = [None] * len(f_files)
+            for i, j in enumerate(f_files):
+                try:
+                    f_labels[i] = re.search(f_re, j).group('label')
+                except IndexError:
+                    f_labels[i] = ''
+            # Append to all
+            all_files += f_files
+            all_labels += f_labels
+
+        return all_files, all_labels
+
 
 class CSVFileDataset(FileDataset):
-    """A dataset read from a CSV file. The file can contain labels, which
-    will be returned in a separate tensor. The labels should be stored
-    in the first ``num_labels`` columns of the CSV file.
+    """
+    A dataset read from a CSV file. The file can contain labels, which will be
+    returned in a separate tensor. The labels should be stored in the first
+    ``num_labels`` columns of the CSV file.
 
-    If ``num_labels>0``, the data is returned as a tuple of tensors
-    ``(labels, samples)``, where ``labels`` is a tensor of shape
-    ``[batch_size, num_labels]``, containing the first ``num_labels`` columns
-    and ``samples`` is a tensor ``[batch_size, ?]`` containing the data samples.
-    If ``num_labels==0``, the data is returned as a single tensor ``samples``.
+    If ``num_labels>0``, the data is returned as a tuple of tensors ``(labels,
+    samples)``, where ``labels`` is a tensor of shape ``[batch_size,
+    num_labels]``, containing the first ``num_labels`` columns and ``samples``
+    is a tensor ``[batch_size, ?]`` containing the data samples. If
+    ``num_labels==0``, the data is returned as a single tensor ``samples``.
 
     This dataset can be overridden to customize the way the data is processed
-    grouped and cast. For instance, to divide the batch into three tensors,
-    with different dtypes in different columns, define custom dataset::
+    grouped and cast. For instance, to divide the batch into three tensors, with
+    different dtypes in different columns, define custom dataset::
 
         class CustomCSVFileDataset(spn.CSVFileDataset):
 
@@ -103,11 +181,9 @@ class CSVFileDataset(FileDataset):
 
     Args:
         files (str or list): A string containing a path to a file or a glob
-                             matching multiple files, or a list of paths
-                             to multiple files. Note that the order of files,
-                             when using a glob is not predictable (even with
-                             ``shuffle`` set to ``False``), but will be constant
-                             across epochs.
+                             matching multiple files, or a list of paths to
+                             multiple files. When glob is used, the files will
+                             be sorted, unless ``shuffle`` is set to ``True``.
         num_epochs (int): Number of epochs of produced data.
         batch_size (int): Size of a single batch.
         shuffle (bool): Shuffle data within each epoch.
