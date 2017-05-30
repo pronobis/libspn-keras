1d6ebcd9bccf5e8ae87e5119e5e198f35dd7baa6
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-30 13:38:18 -0700

    New contraction tests

--- libspn/tests/perf_contraction.py
+++ libspn/tests/perf_contraction.py
@@ -33,11 +33,18 @@ class Ops:
         return tf.tensordot(a, b, axes=[[1], [1]])
 
     def reduction(a, b):
-        n = (-1, 1, b.shape.as_list()[1])
-        return tf.reduce_sum(tf.reshape(a, shape=n) * b, axis=2)
+        return tf.reduce_sum(tf.expand_dims(a, axis=1) * b, axis=2)
 
     def matmul(a, b):
-        return tf.matmul(a, b, transpose_b=True)
+        if len(b.shape) == 2:
+            return tf.matmul(a, b, transpose_b=True)
+        else:
+            return tf.squeeze(tf.matmul(tf.expand_dims(a, axis=1), b, transpose_b=True))
+
+    def reduction_by_matmul(a, b):
+        bcasted = tf.expand_dims(a, axis=1) * b
+        ones = tf.ones(shape=(tf.shape(a)[0], 1, a.shape.as_list()[1]), dtype=bcasted.dtype)
+        return tf.squeeze(tf.matmul(ones, bcasted, transpose_b=True))
 
 
 class OpTestResult:
@@ -123,7 +130,8 @@ class PerformanceTest:
         print2("--> %s: on_gpu=%s, a_shape=%s, b_shape=%s"
                % (op_name, on_gpu, a.shape, b.shape),
                self.file)
-        true_out = a@b.transpose()
+        # true_out = a@b.transpose()
+        true_out = np.sum(a.reshape(a.shape[0], 1, -1) * b, axis=2)
         # Create graph
         tf.reset_default_graph()
         with tf.device(device_name):
@@ -134,7 +142,10 @@ class PerformanceTest:
             # which is served the input data and connected to all ops
             a_pl = tf.placeholder(dtype=self.dtype, shape=(None, a.shape[1]))
             a_op = tf.identity(a_pl)
-            b_pl = tf.placeholder(dtype=self.dtype, shape=(None, b.shape[1]))
+            if b.ndim == 2:
+                b_pl = tf.placeholder(dtype=self.dtype, shape=(None, b.shape[1]))
+            else:
+                b_pl = tf.placeholder(dtype=self.dtype, shape=(None, b.shape[1], b.shape[2]))
             b_op = tf.identity(b_pl)
             # Create ops
             start_time = time.time()
@@ -184,8 +195,15 @@ class PerformanceTest:
         results = []
         a = np.random.rand(self.num_a_rows, self.num_a_cols)
         b = np.random.rand(self.num_b_rows, self.num_a_cols)
-        r = self._run_test('case1',
-                           [Ops.reduction, Ops.matmul],
+        r = self._run_test('case1_2d (simulate multiplication with weights)',
+                           [Ops.reduction, Ops.matmul, Ops.reduction_by_matmul],
+                           a, b)
+        results.append(r)
+
+        a = np.random.rand(self.num_a_rows, self.num_a_cols)
+        b = np.random.rand(self.num_a_rows, self.num_b_rows, self.num_a_cols)
+        r = self._run_test('case2_3d (simulate multiplication with IVs)',
+                           [Ops.reduction, Ops.matmul, Ops.reduction_by_matmul],
                            a, b)
         results.append(r)
 
