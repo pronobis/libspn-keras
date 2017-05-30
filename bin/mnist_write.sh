cbbe45666b0ea4cd101e48f5725505db5b714e9a
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-30 09:54:25 -0700

    MNIST scripts

--- bin/mnist_write.sh
+++ bin/mnist_write.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+if [ "$#" -ne 1 ]
+then
+    echo "Usage: $0 <data_conf_yaml>"
+    exit 1
+fi
+data_conf_yaml="$1"
+
 mkdir -p ./mnist
-./spn-data -v -c mnist_bin_small.yaml read write image "./mnist/%n_%l.png"
+./spn-data -v -c "$data_conf_yaml" read write image "./mnist/%n_%l.png"
