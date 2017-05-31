18c6cb5f88311644aac528aa570ce69892d54498
Andrzej Pronobis <a.pronobis@gmail.com>
2017-05-30 22:16:14 -0700

    Updated tests

--- libspn/tests/test_data_file.py
+++ libspn/tests/test_data_file.py
@@ -29,82 +29,163 @@ class TestFileDataset(tf.test.TestCase):
         else:
             return os.path.join(TestFileDataset.data_dir, p)
 
+    @patch.multiple(spn.FileDataset, __abstractmethods__=set())  # Make Dataset non-abstract
     def test_get_files_labels(self):
         """Obtaining files/labels list from a specification in FileDataset"""
         def run(paths, true_files, true_labels):
             with self.subTest(paths=paths):
-                files, labels = spn.FileDataset._get_files_labels(
-                    self.data_path(paths))
+                dataset = spn.FileDataset(files=self.data_path(paths),
+                                          num_epochs=1,
+                                          batch_size=1,
+                                          shuffle=False,
+                                          shuffle_batch=False)
+                files, labels = dataset._get_files_labels()
                 self.assertEqual(files, self.data_path(true_files))
                 self.assertEqual(labels, true_labels)
 
         # Single path without glob and label
-        run("img_dir1/img1-A.png",
-            ["img_dir1/img1-A.png"],
+        run('img_dir1/img1-A.png',
+            ['img_dir1/img1-A.png'],
             [''])
 
         # Single path with glob and no label
-        run("img_dir*/img*-*.png",
-            ["img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png",
-             "img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir3/img1-A.png",
-             "img_dir3/img2-C.png",
-             "img_dir3/img3-B.png",
-             "img_dir4/img1-A.png",
-             "img_dir4/img2-C.png",
-             "img_dir4/img3-B.png"],
+        run('img_dir*/img*-*.png',
+            ['img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png',
+             'img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir3/img1-A.png',
+             'img_dir3/img2-C.png',
+             'img_dir3/img3-B.png',
+             'img_dir4/img1-A.png',
+             'img_dir4/img2-C.png',
+             'img_dir4/img3-B.png'],
             ['', '', '', '', '', '', '', '', '', '', '', ''])
 
         # Single path with a glob and a label
-        run("img_dir*/img*-{*}.png",
-            ["img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png",
-             "img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir3/img1-A.png",
-             "img_dir3/img2-C.png",
-             "img_dir3/img3-B.png",
-             "img_dir4/img1-A.png",
-             "img_dir4/img2-C.png",
-             "img_dir4/img3-B.png"],
+        run('img_dir*/img*-{*}.png',
+            ['img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png',
+             'img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir3/img1-A.png',
+             'img_dir3/img2-C.png',
+             'img_dir3/img3-B.png',
+             'img_dir4/img1-A.png',
+             'img_dir4/img2-C.png',
+             'img_dir4/img3-B.png'],
             ['A', 'C', 'B', 'A', 'C', 'B', 'A', 'C', 'B', 'A', 'C', 'B'])
 
         # Multiple paths without glob and label
         # Tests if order of files is preserved
-        run(["img_dir2/img2-C.png",
-             "img_dir1/img1-A.png"],
-            ["img_dir2/img2-C.png",
-             "img_dir1/img1-A.png"],
+        run(['img_dir2/img2-C.png',
+             'img_dir1/img1-A.png'],
+            ['img_dir2/img2-C.png',
+             'img_dir1/img1-A.png'],
             ['', ''])
 
         # Multiple paths with glob and no label
         # Tests if order of file specs is preserved
-        run(["img_dir2/img*-*.png",
-             "img_dir1/img*-*.png"],
-            ["img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png"],
+        run(['img_dir2/img*-*.png',
+             'img_dir1/img*-*.png'],
+            ['img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png'],
             ['', '', '', '', '', ''])
 
         # Multiple paths with a glob and a label
-        run(["img_dir2/img*-{*}.png",
-             "img_dir1/img*-{*}.png"],
-            ["img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png"],
+        run(['img_dir2/img*-{*}.png',
+             'img_dir1/img*-{*}.png'],
+            ['img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png'],
             ['A', 'C', 'B', 'A', 'C', 'B'])
+
+    @patch.multiple(spn.FileDataset, __abstractmethods__=set())  # Make Dataset non-abstract
+    def test_get_files_labels_classes(self):
+        """Obtaining files/labels list from a specification in FileDataset
+        for specific class labels."""
+        def run(paths, classes, true_files, true_labels):
+            with self.subTest(paths=paths, classes=classes):
+                dataset = spn.FileDataset(files=self.data_path(paths),
+                                          num_epochs=1,
+                                          batch_size=1,
+                                          shuffle=False,
+                                          shuffle_batch=False,
+                                          classes=classes)
+                files, labels = dataset._get_files_labels()
+                self.assertEqual(files, self.data_path(true_files))
+                self.assertEqual(labels, true_labels)
+
+        # Single path without glob and label
+        run('img_dir1/img1-A.png',
+            ['A'],
+            [], [])
+
+        # Single path with glob and no label
+        run('img_dir*/img*-*.png',
+            ['A'],
+            [], [])
+
+        # Single path with a glob and a label
+        run('img_dir*/img*-{*}.png',
+            ['C'],
+            ['img_dir1/img2-C.png',
+             'img_dir2/img2-C.png',
+             'img_dir3/img2-C.png',
+             'img_dir4/img2-C.png'],
+            ['C', 'C', 'C', 'C'])
+        run('img_dir*/img*-{*}.png',
+            ['B', 'A'],
+            ['img_dir1/img1-A.png',
+             'img_dir1/img3-B.png',
+             'img_dir2/img1-A.png',
+             'img_dir2/img3-B.png',
+             'img_dir3/img1-A.png',
+             'img_dir3/img3-B.png',
+             'img_dir4/img1-A.png',
+             'img_dir4/img3-B.png'],
+            ['A', 'B', 'A', 'B', 'A', 'B', 'A', 'B'])
+
+        # Multiple paths without glob and label
+        # Tests if order of files is preserved
+        run(['img_dir2/img2-C.png',
+             'img_dir1/img1-A.png'],
+            ['A'],
+            [], [])
+
+        # Multiple paths with glob and no label
+        # Tests if order of file specs is preserved
+        run(['img_dir2/img*-*.png',
+             'img_dir1/img*-*.png'],
+            ['A'],
+            [], [])
+
+        # Multiple paths with a glob and a label
+        run(['img_dir2/img*-{*}.png',
+             'img_dir1/img*-{*}.png'],
+            ['B'],
+            ['img_dir2/img3-B.png',
+             'img_dir1/img3-B.png'],
+            ['B', 'B'])
+        run(['img_dir2/img*-{*}.png',
+             'img_dir1/img*-{*}.png'],
+            ['C', 'A'],
+            ['img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir1/img1-A.png',
+             'img_dir1/img2-C.png'],
+            ['A', 'C', 'A', 'C'])
 
     @patch.multiple(spn.FileDataset, __abstractmethods__=set())  # Make Dataset non-abstract
     def test_file_label_serving(self):
@@ -133,71 +214,71 @@ class TestFileDataset(tf.test.TestCase):
                 self.assertEqual(labels, true_labels)
 
         # Single path without glob and label
-        run("img_dir1/img1-A.png",
-            ["img_dir1/img1-A.png"],
+        run('img_dir1/img1-A.png',
+            ['img_dir1/img1-A.png'],
             [''])
 
         # Single path with glob and no label
-        run("img_dir*/img*-*.png",
-            ["img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png",
-             "img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir3/img1-A.png",
-             "img_dir3/img2-C.png",
-             "img_dir3/img3-B.png",
-             "img_dir4/img1-A.png",
-             "img_dir4/img2-C.png",
-             "img_dir4/img3-B.png"],
+        run('img_dir*/img*-*.png',
+            ['img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png',
+             'img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir3/img1-A.png',
+             'img_dir3/img2-C.png',
+             'img_dir3/img3-B.png',
+             'img_dir4/img1-A.png',
+             'img_dir4/img2-C.png',
+             'img_dir4/img3-B.png'],
             ['', '', '', '', '', '', '', '', '', '', '', ''])
 
         # Single path with a glob and a label
-        run("img_dir*/img*-{*}.png",
-            ["img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png",
-             "img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir3/img1-A.png",
-             "img_dir3/img2-C.png",
-             "img_dir3/img3-B.png",
-             "img_dir4/img1-A.png",
-             "img_dir4/img2-C.png",
-             "img_dir4/img3-B.png"],
+        run('img_dir*/img*-{*}.png',
+            ['img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png',
+             'img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir3/img1-A.png',
+             'img_dir3/img2-C.png',
+             'img_dir3/img3-B.png',
+             'img_dir4/img1-A.png',
+             'img_dir4/img2-C.png',
+             'img_dir4/img3-B.png'],
             ['A', 'C', 'B', 'A', 'C', 'B', 'A', 'C', 'B', 'A', 'C', 'B'])
 
         # Multiple paths without glob and label
         # Tests if order of files is preserved
-        run(["img_dir2/img2-C.png",
-             "img_dir1/img1-A.png"],
-            ["img_dir2/img2-C.png",
-             "img_dir1/img1-A.png"],
+        run(['img_dir2/img2-C.png',
+             'img_dir1/img1-A.png'],
+            ['img_dir2/img2-C.png',
+             'img_dir1/img1-A.png'],
             ['', ''])
 
         # Multiple paths with glob and no label
         # Tests if order of file specs is preserved
-        run(["img_dir2/img*-*.png",
-             "img_dir1/img*-*.png"],
-            ["img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png"],
+        run(['img_dir2/img*-*.png',
+             'img_dir1/img*-*.png'],
+            ['img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png'],
             ['', '', '', '', '', ''])
 
         # Multiple paths with a glob and a label
-        run(["img_dir2/img*-{*}.png",
-             "img_dir1/img*-{*}.png"],
-            ["img_dir2/img1-A.png",
-             "img_dir2/img2-C.png",
-             "img_dir2/img3-B.png",
-             "img_dir1/img1-A.png",
-             "img_dir1/img2-C.png",
-             "img_dir1/img3-B.png"],
+        run(['img_dir2/img*-{*}.png',
+             'img_dir1/img*-{*}.png'],
+            ['img_dir2/img1-A.png',
+             'img_dir2/img2-C.png',
+             'img_dir2/img3-B.png',
+             'img_dir1/img1-A.png',
+             'img_dir1/img2-C.png',
+             'img_dir1/img3-B.png'],
             ['A', 'C', 'B', 'A', 'C', 'B'])
 
 
