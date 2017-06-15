c0779720e389b66e76a8809e884874a47f286ce9
Avinash Ranganath <nash911@gmail.com>
2017-06-15 20:04:39 +0200

    Modify performance test with multiple IVs and for calculating all weights counts

--- libspn/tests/perf_sum_path.py
+++ libspn/tests/perf_sum_path.py
@@ -281,7 +281,7 @@ class PerformanceTest:
                     output_correct = False
                     self.test_failed = True
 
-            if all([self.profile, indices is not None, ivs is not None]):
+            if self.profile:
                 # Add additional options to trace the session execution
                 options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
                 run_metadata = tf.RunMetadata()
