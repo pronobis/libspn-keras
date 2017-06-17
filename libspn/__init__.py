02a72878014fe1766dddd789f5764c7fe031808a
Avinash Ranganath <nash911@gmail.com>
2017-06-18 00:52:24 +0200

    Add node PermProducts

--- libspn/__init__.py
+++ libspn/__init__.py
@@ -19,6 +19,7 @@ from libspn.graph.contvars import ContVars
 from libspn.graph.concat import Concat
 from libspn.graph.sum import Sum
 from libspn.graph.product import Product
+from libspn.graph.permproducts import PermProducts
 from libspn.graph.weights import Weights
 from libspn.graph.weights import assign_weights
 from libspn.graph.weights import initialize_weights
@@ -100,7 +101,7 @@ from libspn.exceptions import StructureError
 __all__ = [
     # Graph
     'Scope', 'Input', 'Node', 'ParamNode', 'OpNode', 'VarNode',
-    'Concat', 'IVs', 'ContVars', 'Sum', 'Product',
+    'Concat', 'IVs', 'ContVars', 'Sum', 'Product', 'PermProducts',
     'Weights', 'assign_weights', 'initialize_weights',
     'serialize_graph', 'deserialize_graph',
     'Saver', 'Loader', 'JSONSaver', 'JSONLoader',
