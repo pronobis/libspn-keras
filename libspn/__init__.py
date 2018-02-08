5912e18d5bd37f22f5af57cce22076eb131f3a9b
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-02-08 15:04:45 +0100

    added initial sums layer class that at least passes tests for previous sums class

--- libspn/__init__.py
+++ libspn/__init__.py
@@ -20,6 +20,7 @@ from libspn.graph.concat import Concat
 from libspn.graph.sum import Sum
 from libspn.graph.parsums import ParSums
 from libspn.graph.sums import Sums
+from libspn.graph.sumslayer import SumsLayer
 from libspn.graph.product import Product
 from libspn.graph.permproducts import PermProducts
 from libspn.graph.products import Products
@@ -106,7 +107,7 @@ __all__ = [
     # Graph
     'Scope', 'Input', 'Node', 'ParamNode', 'OpNode', 'VarNode',
     'Concat', 'IVs', 'ContVars',
-    'Sum', 'ParSums', 'Sums',
+    'Sum', 'ParSums', 'Sums', 'SumsLayer',
     'Product', 'PermProducts', 'Products',
     'Weights', 'assign_weights', 'initialize_weights',
     'serialize_graph', 'deserialize_graph',
