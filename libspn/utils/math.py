33d79ec959c04c13a8fe20662bc22adafed28508
Avinash Ranganath <nash911@gmail.com>
2018-02-13 15:11:32 +0100

    .

--- libspn/utils/math.py
+++ libspn/utils/math.py
@@ -84,7 +84,7 @@ def gather_cols(params, indices, name=None):
         if np.any((indices < 0) | (indices >= param_size)):
             raise ValueError("'indices' must fit the the indexed dimension")
         # Define op
-        if param_size == 1:# and indices.size == 1:
+        if param_size == 1:
             if indices.size == 1:
                 # Single column tensor with a single indices, which should include
                 # it, so just forward tensor
