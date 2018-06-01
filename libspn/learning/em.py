5b4acdd34b04d6ef2612f85fdfcfc68b0c2981f3
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-06-01 16:44:06 +0200

    added dropout gradient support

--- libspn/learning/em.py
+++ libspn/learning/em.py
@@ -31,7 +31,8 @@ class EMLearning():
 
     def __init__(self, root, mpe_path=None, log=True, value_inference_type=None,
                  additive_smoothing=None, add_random=None, initial_accum_value=None,
-                 use_unweighted=False, sample=False, sample_prob=None):
+                 use_unweighted=False, sample=False, sample_prob=None, sample_rank_based=None,
+                 dropout_keep_prob=None, dropconnect_keep_prob=None):
         self._root = root
         self._log = log
         self._additive_smoothing = additive_smoothing
@@ -39,10 +40,11 @@ class EMLearning():
         self._sample = sample
         # Create internal MPE path generator
         if mpe_path is None:
-            self._mpe_path = MPEPath(log=log,
-                                     value_inference_type=value_inference_type,
-                                     add_random=add_random, use_unweighted=use_unweighted,
-                                     sample=sample, sample_prob=sample_prob)
+            self._mpe_path = MPEPath(
+                log=log, value_inference_type=value_inference_type, add_random=add_random,
+                use_unweighted=use_unweighted, sample=sample, sample_prob=sample_prob,
+                sample_rank_based=sample_rank_based, dropout_keep_prob=dropout_keep_prob,
+                dropconnect_keep_prob=dropconnect_keep_prob)
         else:
             self._mpe_path = mpe_path
         # Create a name scope
