4c37f1a4ca5cfbe61b986e8800209f2fca13aa1c
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-06-29 10:37:57 +0200

    changed to NCHW to avoid TensorFlow bug

--- libspn/tests/test_graph_convprod.py
+++ libspn/tests/test_graph_convprod.py
@@ -5,8 +5,146 @@ import numpy as np
 import libspn as spn
 from libspn.log import get_logger
 from libspn.generation.spatial import ConvSPN
-
+import itertools
+import random
 logger = get_logger()
+
+
+def generate_sparse_connections(num_in_channels, num_out_channels, kernel_size):
+    kernel_surface = int(np.prod(kernel_size))
+    total_possibilities = num_in_channels ** kernel_surface
+    if num_out_channels >= total_possibilities:
+        p = np.arange(total_possibilities)
+        kernel_cells = []
+        for _ in range(kernel_surface):
+            kernel_cells.append(p % num_in_channels)
+            p //= num_in_channels
+        return np.stack(kernel_cells, axis=0).reshape(kernel_size + [total_possibilities])\
+            .transpose((2, 0, 1))
+
+    sparse_shape = kernel_size + [num_out_channels]
+    size = int(np.prod(sparse_shape))
+    return np.random.randint(num_in_channels, size=size).reshape(sparse_shape).transpose((2, 0, 1))
+
+
+def grid_spn(var_node, patch_size, num_mixtures, num_alterations, input_dist, num_rows, num_cols,
+             max_patch_combs=128, asymmetric=False, conv=False, dilation_rate=1, strides=2):
+
+    if isinstance(var_node, spn.GaussianLeaf):
+        # TODO num_components should be property
+        last_dim_size = var_node._num_components
+    else:
+        last_dim_size = var_node.num_vals
+
+    def sub2ind(r, c, elem):
+        return r * num_cols * last_dim_size + c * last_dim_size + elem
+
+    def _patch_combinations(patch_size, last_dim_size):
+        num_scopes = int(np.prod(patch_size))
+        num_combinations = last_dim_size ** num_scopes
+
+        if num_combinations > max_patch_combs:
+            combinations = np.random.choice(num_combinations, size=max_patch_combs, replace=False)
+            unique_patch_combinations = []
+            for c in combinations:
+                combin = []
+                for i in range(num_scopes):
+                    combin.append(int(c % last_dim_size))
+                    c //= last_dim_size
+                unique_patch_combinations.append(tuple(combin))
+            assert len(set(unique_patch_combinations)) == max_patch_combs
+        else:
+            # unique_patch_combinations = list(itertools.product(
+            #     *[range(last_dim_size) for _ in range(np.product(patch_size))]))
+            unique_patch_combinations = generate_sparse_connections(
+                last_dim_size, num_combinations, kernel_size=list(patch_size)).reshape(
+                (-1, int(np.prod(patch_size)))).astype(int).tolist()
+        return unique_patch_combinations
+
+    if isinstance(num_mixtures, int):
+        num_mixtures = [num_mixtures]
+    if len(num_mixtures) == 1:
+        num_mixtures *= num_alterations
+
+    offsets = list(itertools.product(range(patch_size[0]), range(patch_size[1])))
+
+    product_layer = input_dist == spn.DenseSPNGeneratorLayerNodes.InputDist.RAW
+    if product_layer:
+        unique_patch_combinations = _patch_combinations(patch_size, last_dim_size)
+        print("Unique patch combinations")
+        print(unique_patch_combinations)
+    else:
+        unique_patch_combinations = []
+    node = var_node
+
+    num_layers = num_alterations * 2 - (1 if asymmetric else 0)
+    for layer_ind in range(num_layers):
+        # Go over all grid positions
+        print("Generating grid for layer {}, [{} x {} x {}]".format(
+            layer_ind, num_rows, num_cols,
+            node.get_out_size() // (num_cols * num_rows)))
+
+        num_mixt = num_mixtures[layer_ind // 2]
+
+        out_rows = int(np.ceil((num_rows - (patch_size[0] - 1) * dilation_rate) / strides))
+        out_cols = int(np.ceil((num_cols - (patch_size[1] - 1) * dilation_rate) / strides))
+
+        indices = []
+        print("Dilation Rate", dilation_rate)
+        for row in range(out_rows):
+            for col in range(out_cols):
+                if product_layer:
+                    # print(row, col, len(unique_patch_combinations))
+                    for patch_comb in unique_patch_combinations:
+                        patch_indices = []
+                        assert len(offsets) == len(patch_comb), \
+                            "Length of offsets and patch combinations do not match... {} vs. {}" \
+                            "".format(len(offsets), len(patch_comb))
+                        for (dr, dc), elem in zip(offsets, patch_comb):
+                            ind = sub2ind(row * strides + dr * dilation_rate,
+                                          col * strides + dc * dilation_rate, elem)
+                            patch_indices.append(ind)
+                            # print(row + dr * dilation_rate, col + dc * dilation_rate, end=", ")
+                        # print("\n")
+                        indices.extend(patch_indices)
+                else:  # sums_layer
+                    mixture_indices = []
+                    # print(row, col)
+                    for e in range(last_dim_size):
+                        ind = sub2ind(row, col, e)
+                        mixture_indices.append(ind)
+                    for _ in range(num_mixt):
+                        indices.extend(mixture_indices)
+
+        last_dim_size = len(unique_patch_combinations) if product_layer else num_mixt
+        print("Constructing layer")
+        if product_layer:
+            # if dilation_rate == 1:
+            num_rows = int(np.ceil((num_rows - (patch_size[0] - 1) * dilation_rate) / strides))
+            num_cols = int(np.ceil((num_cols - (patch_size[1] - 1) * dilation_rate) / strides))
+
+            print("Layer out size {} x {}".format(num_rows, num_cols))
+            print("Size per prod {}".format(len(indices) / (num_rows * num_cols * last_dim_size)))
+            node = spn.ProductsLayer(
+                (node, indices), name="GridProducts{}".format(layer_ind),
+                num_or_size_prods=num_rows * num_cols * last_dim_size)
+        else:
+            if conv:
+                node = spn.ConvSum(node, num_channels=num_mixt, grid_dim_sizes=[num_rows, num_cols])
+            else:
+                node = spn.SumsLayer(
+                    (node, indices), name="GridSums{}".format(layer_ind),
+                    num_or_size_sums=num_rows * num_cols * last_dim_size)
+            if layer_ind != num_layers - 1:
+                unique_patch_combinations = _patch_combinations(patch_size, last_dim_size)
+
+        print("Added layer {}".format(node.name))
+        # Switch to other layer type
+        product_layer = not product_layer
+
+    print("Built grid spn")
+    return node
+
 
 class TestConvProd(tf.test.TestCase):
 
@@ -78,7 +216,69 @@ class TestConvProd(tf.test.TestCase):
         with self.test_session() as sess:
             conv_out = sess.run(conv_op)
 
-        self.assertAllClose(conv_out, np.ones((2, 2)) * 4)
+        self.assertAllClose(conv_out, np.ones((1, 2, 2, 1)) * 4)
+
+    @argsprod([False, True])
+    def test_compare_manual_convprod(self, stride):
+        spn.conf.argmax_zero = True
+        grid_dims = [4, 4]
+        batch_size = 256
+        num_vars = grid_dims[0] * grid_dims[1]
+        ivs = spn.IVs(num_vars=num_vars, num_vals=2)
+
+        dilation_rate = 1 if stride else 2
+        stride = 2 if stride else 1
+
+        conv_manual = grid_spn(ivs, patch_size=(2, 2), num_mixtures=[2], num_alterations=1,
+                               asymmetric=True, num_rows=grid_dims[0], num_cols=grid_dims[1],
+                               max_patch_combs=16,
+                               input_dist=spn.DenseSPNGeneratorLayerNodes.InputDist.RAW,
+                               strides=stride, dilation_rate=dilation_rate)
+        conv_layer = spn.ConvProd2D(ivs, kernel_size=2, num_channels=16, grid_dim_sizes=grid_dims,
+                                    strides=stride, dilation_rate=dilation_rate)
+
+        dense_gen = spn.DenseSPNGeneratorLayerNodes(
+            num_decomps=1, num_subsets=2, num_mixtures=2,
+            input_dist=spn.DenseSPNGeneratorLayerNodes.InputDist.MIXTURE,
+            node_type=spn.DenseSPNGeneratorLayerNodes.NodeType.LAYER)
+
+        rnd = random.Random(x=1234)
+        rnd_state = rnd.getstate()
+        root_manual = dense_gen.generate(conv_manual, root_name="RootManual", rnd=rnd)
+        spn.generate_weights(root_manual)
+        init_manual = spn.initialize_weights(root_manual)
+
+        rnd.setstate(rnd_state)
+        root_layer = dense_gen.generate(conv_layer, root_name="RootLayer", rnd=rnd)
+        spn.generate_weights(root_layer)
+        init_layer = spn.initialize_weights(root_layer)
+
+        pathgen_man = spn.MPEPath(value_inference_type=spn.InferenceType.MARGINAL, log=True)
+        pathgen_man.get_mpe_path(root_manual)
+
+        pathgen_lay = spn.MPEPath(value_inference_type=spn.InferenceType.MARGINAL, log=True)
+        pathgen_lay.get_mpe_path(root_layer)
+
+        ivs_counts_man = pathgen_man.counts[ivs]
+        ivs_counts_lay = pathgen_lay.counts[ivs]
+
+        value_man = pathgen_man.value.values[root_manual]
+        value_lay = pathgen_lay.value.values[root_layer]
+
+        random_ivs_feed = np.random.randint(-1, 2, size=num_vars * batch_size).reshape(
+            (batch_size, num_vars))
+
+        with self.test_session() as sess:
+            sess.run([init_manual, init_layer])
+
+            counts_man_out, counts_lay_out = sess.run(
+                [ivs_counts_man, ivs_counts_lay], feed_dict={ivs: random_ivs_feed})
+
+            value_man_out, value_lay_out = sess.run(
+                [value_man, value_lay], feed_dict={ivs: random_ivs_feed})
+
+        self.assertAllClose(np.exp(value_man_out), np.exp(value_lay_out))
+        self.assertAllEqual(counts_man_out, counts_lay_out)
 
     def test_compute_mpe_path(self):
         grid_dims = [4, 4]
@@ -117,6 +317,29 @@ class TestConvProd(tf.test.TestCase):
             var_counts_out = sess.run(var_counts, feed_dict={vars: np.random.rand(1, 4 * 4 * 2)})
 
         self.assertAllClose(truth, var_counts_out)
+
+    def test_compute_mpe_path_dilated(self):
+        grid_dims = [4, 4]
+        input_channels = 2
+        vars = spn.ContVars(num_vars=grid_dims[0] * grid_dims[1] * input_channels)
+        convprod = spn.ConvProd2D(vars, num_channels=32, padding_algorithm='valid', strides=1,
+                                  grid_dim_sizes=grid_dims, dilation_rate=2)
+
+        valgen = spn.LogValue(inference_type=spn.InferenceType.MARGINAL)
+        valgen.get_value(convprod)
+
+        counts = np.stack([
+            np.arange(16) + 23 * i for i in range(4)]).reshape((1, 2, 2, 16)).astype(np.float32)
+
+        var_counts = tf.reshape(
+            convprod._compute_log_mpe_path(counts, valgen.values[vars])[0],
+            (1, 4, 4, 2))
+
+        with self.test_session() as sess:
+            var_counts_out = sess.run(var_counts, feed_dict={vars: np.random.rand(1, 4 * 4 * 2)})
+
+        print(var_counts_out)
+        # self.assertAllClose(truth, var_counts_out)
 
     def test_compute_value_padding(self):
         grid_dims = [2, 2]
