ee1e9d522de283459ebc0ccca743cf3ede7f6949
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-07-11 13:08:08 +0200

    added spatial perm products node

--- libspn/__init__.py
+++ libspn/__init__.py
@@ -28,6 +28,7 @@ from libspn.graph.permproducts import PermProducts
 from libspn.graph.products import Products
 from libspn.graph.productslayer import ProductsLayer
 from libspn.graph.convprod2d import ConvProd2D, ConvProd2DV2
+from libspn.graph.spatialpermproducts import SpatialPermProducts
 from libspn.graph.stridedslice import StridedSlice2D
 from libspn.graph.weights import Weights
 from libspn.graph.weights import assign_weights
@@ -118,8 +119,8 @@ __all__ = [
     'Scope', 'Input', 'Node', 'ParamNode', 'OpNode', 'VarNode',
     'Concat', 'IVs', 'ContVars',
     'Sum', 'ParSums', 'Sums', 'SumsLayer', 'ConvSum',
-    'Product', 'PermProducts', 'Products', 'ProductsLayer', 'ConvProd2D', 'ConvProd2DV2',
-    'GaussianLeaf',
+    'Product', 'PermProducts', 'Products', 'ProductsLayer', 
+    'ConvProd2D', 'ConvProd2DV2', 'GaussianLeaf',
     'Weights', 'assign_weights', 'initialize_weights',
     'serialize_graph', 'deserialize_graph',
     'Saver', 'Loader', 'JSONSaver', 'JSONLoader',
