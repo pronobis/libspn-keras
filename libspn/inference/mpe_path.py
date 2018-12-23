d4627aeb0802c2ff06531db68969e24abb5e799f
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-12-23 16:20:48 +0100

    remove printing statements

--- libspn/inference/mpe_path.py
+++ libspn/inference/mpe_path.py
@@ -99,8 +99,6 @@ class MPEPath:
                 # Compute for inputs
                 with tf.name_scope(node.name):
                     if self._log:
-                        print(node)
-                        print(summed, [self._value.values[i.node] if i else None for i in node.inputs])
                         return node._compute_log_mpe_path(
                             summed, *[self._value.values[i.node] if i else None
                                       for i in node.inputs], **kwargs)
@@ -115,8 +113,6 @@ class MPEPath:
 
         with tf.name_scope("TrueMPEPath"):
             # Compute the tensor to feed to the root node
-            for k, v in self._value.values.items():
-                print(k, v)
             graph_input = tf.ones_like(self._value.values[root])
 
             # Traverse the graph computing counts
@@ -159,8 +155,6 @@ class MPEPath:
             self._value.get_value(root)
 
         with tf.name_scope("ActualMPEPath"):
-            for k, v in self._value.values.items():
-                print(k, v)
             graph_input = tf.ones_like(self._value.values[root])
 
             # Traverse the graph computing counts
