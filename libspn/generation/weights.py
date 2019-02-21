last fixes in test and remove ValueType and broadcast_value altogether

libspn/generation/weights.py
@@ -18,8 +18,7 @@ class WeightsGenerator:
     otherwise the number of weight values will be incorrect.
 
     Attributes:
-        initializer: Initial value of the weights. For possible values, see
-                    :meth:`~libspn.utils.broadcast_value`.
+        initializer: Initial value of the weights.
         trainable: See :class:`~libspn.Weights`.
         log (bool): If "True", the weights are represented in log space.
     """
@@ -61,8 +60,7 @@ def generate_weights(root, initializer=tf.initializers.constant(1.0), trainable=
 
     Args:
         root (Node): The root node of the SPN graph.
-        init_value: Initial value of the weights. For possible values, see
-                    :meth:`~libspn.utils.broadcast_value`.
+        init_value: Initial value of the weights.
         trainable: See :class:`~libspn.Weights`.
         log (bool): If "True", the weights are represented in log space.
     """

libspn/tests/test_graph_parsums.py
@@ -1217,12 +1217,12 @@ class TestNodesParSums(tf.test.TestCase):
         w = s.generate_weights()
         counts = tf.placeholder(tf.float32, shape=(None, 2))
         op = s._compute_log_mpe_path(tf.identity(counts),
-                                 w.get_value(),
-                                 iv.get_value(),
-                                 v12.get_value(),
-                                 v34.get_value(),
-                                 v12.get_value(),
-                                 v5.get_value())
+                                 w.get_log_value(),
+                                 iv.get_log_value(),
+                                 v12.get_log_value(),
+                                 v34.get_log_value(),
+                                 v12.get_log_value(),
+                                 v5.get_log_value())
         init = w.initialize()
         counts_feed = [[10, 20],
                        [11, 21],
@@ -1240,7 +1240,7 @@ class TestNodesParSums(tf.test.TestCase):
                     [1, 1],
                     [0, 0],
                     [3, 3]]
-        v34_feed = [[0.1, 0.2],
+        v34_feed = [[0.1, 0.2],  # 0.2 
                     [1.2, 0.2],
                     [0.1, 0.2],
                     [0.9, 0.8],

libspn/utils/math.py
@@ -10,40 +10,7 @@
 import tensorflow as tf
 import numpy as np
 import collections
-from libspn import conf, utils as utils
-from libspn.utils.serialization import register_serializable
-
-
-class ValueType:
-
-    """A class specifying various types of values that be passed to the SPN
-    graph."""
-
-    @register_serializable
-    class RANDOM_UNIFORM:
-
-        """A random value from a uniform distribution.
-
-        Attributes:
-            min_val: The lower bound of the range of random values.
-            max_val: The upper bound of the range of random values.
-        """
-
-        def __init__(self, min_val=0, max_val=1):
-            self.min_val = min_val
-            self.max_val = max_val
-
-        def __repr__(self):
-            return ("ValueType.RANDOM_UNIFORM(min_val=%s, max_val=%s)" %
-                    (self.min_val, self.max_val))
-
-        def serialize(self):
-            return {'min_val': self.min_val,
-                    'max_val': self.max_val}
-
-        def deserialize(self, data):
-            self.min_val = data['min_val']
-            self.max_val = data['max_val']
+from libspn import utils as utils
 
 
 def gather_cols_3d(params, indices, pad_elem=0, name=None):
@@ -309,39 +276,6 @@ def scatter_values(params, indices, num_out_cols, name=None):
             else:
                 return tf.one_hot(indices, num_out_cols, dtype=params.dtype) \
                        * tf.expand_dims(params, axis=2)
-
-
-def broadcast_value(value, shape, dtype, name=None):
-    """Broadcast the given value to the given shape and dtype. If ``value`` is
-    one of the members of :class:`~libspn.ValueType`, the requested value will
-    be generated and placed in every element of a tensor of the requested shape
-    and dtype. If ``value`` is a 0-D tensor or a Python value, it will be
-    broadcasted to the requested shape and converted to the requested dtype.
-    Otherwise, the value is used as is.
-
-    Args:
-        value: The input value.
-        shape: The shape of the output.
-        dtype: The type of the output.
-
-    Return:
-        Tensor: A tensor containing the broadcasted and converted value.
-    """
-    with tf.name_scope(name, "broadcast_value", [value]):
-        # Recognize ValueTypes
-        if isinstance(value, ValueType.RANDOM_UNIFORM):
-            return tf.random_uniform(shape=shape,
-                                     minval=value.min_val,
-                                     maxval=value.max_val,
-                                     dtype=dtype)
-
-        # Broadcast tensors and scalars
-        tensor = tf.convert_to_tensor(value, dtype=dtype)
-        if tensor.get_shape() == tuple():
-            return tf.fill(dims=shape, value=tensor)
-
-        # Return original input if we cannot broadcast
-        return tensor
 
 
 def print_tensor(*tensors):