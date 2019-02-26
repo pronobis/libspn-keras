20dc15c3b633edfd9e4bf33ddd74cebb63a95e36
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2019-02-26 23:17:36 +0100

    update setup a bit

--- setup.py
+++ setup.py
@@ -9,6 +9,31 @@ def get_readme():
         return f.read()
 
 
+_VERSION = '0.1'
+
+
+_packages = {
+    'tensorflow': 'tensorflow>=1.12.0',
+    'tensorflow-gpu': 'tensorflow-gpu>=1.12.0',
+}
+
+
+EXTRA_PACKAGES = {
+    'tensorflow': [_packages['tensorflow']],
+    'tensorflow with gpu': [_packages['tensorflow-gpu']],
+}
+
+REQUIRED_PACKAGES = [
+    'tqdm',
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'parameterized',
+    'pillow',
+    'pyyaml',
+    'colorama'  # For color output in tests
+]
+
 ###############################
 # Setup
 ###############################
@@ -17,7 +42,9 @@ setup(
     # General Info
     ################
     name='libspn',
-    description='The libsak joke in the world',
+    version=_VERSION,
+    description='LibSPN is a TensorFlow-based library for building and training '
+                'Sum-Product Networks.',
     long_description=get_readme(),
     setup_requires=[
         'setuptools_scm',  # Use version from SCM using setuptools_scm
@@ -34,32 +61,34 @@ setup(
     ],
     use_scm_version=True,  # Use version from SCM using setuptools_scm
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        # 'License :: ', TODO
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache Software License',
     ],
     keywords=('libspn spn deep-learning deep-learning-library '
               'machine-learning machine-learning-library tensorflow'),
     url='http://www.libspn.org',
-    author='Andrzej Pronobis',
+    author='Andrzej Pronobis, Avinash Raganath, Jos van de Wolfshaar',
     author_email='a.pronobis@gmail.com',
-    license='Custom',
+    license='Apache 2.0',
 
     ################
     # Installation
     ################
     packages=['libspn'],
-    install_requires=[
-        # 'tensorflow', Disabled, it overwrites a GPU installation with manulinux from pypi
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'parameterized',
-        'pillow',
-        'pyyaml',
-        'colorama'  # For color output in tests
-    ],
+    install_requires=REQUIRED_PACKAGES,
+    extra_require=EXTRA_PACKAGES,
     zip_safe=False,
     # Stuff in git repo will be included in source dist
     include_package_data=True,
