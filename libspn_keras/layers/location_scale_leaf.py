de05e0fc426d4cb7ac6da110b92ca62e85ee2ddf
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2020-04-01 22:21:43 +0200

    add accumulators location scale leafs

--- libspn_keras/layers/location_scale_leaf.py
+++ libspn_keras/layers/location_scale_leaf.py
@@ -4,6 +4,8 @@ from libspn_keras.layers.base_leaf import BaseLeaf
 import tensorflow_probability as tfp
 from tensorflow import initializers
 
+from libspn_keras.math.soft_em_grads import LocationScaleEMGradWrapper
+
 
 class LocationScaleLeafBase(BaseLeaf):
     """
@@ -14,29 +16,48 @@ class LocationScaleLeafBase(BaseLeaf):
         location_initializer: Initializer for location variable
         location_trainable: Boolean that indicates whether location is trainable
         scale_initializer: Initializer for scale variable
-        scale_trainable: Boolean that indicates whether scale is trainable
         **kwargs: kwargs to pass on to the keras.Layer super class
     """
     def __init__(
         self, num_components, location_initializer=None,
-        location_trainable=True, scale_initializer=None, scale_trainable=False, **kwargs
+        location_trainable=True, scale_initializer=None,
+        accumulator_initializer=None, use_accumulators=False, **kwargs
     ):
         super(LocationScaleLeafBase, self).__init__(num_components=num_components, **kwargs)
         self.location_initializer = location_initializer or initializers.TruncatedNormal(stddev=1.0)
         self.location_trainable = location_trainable
         self.scale_initializer = scale_initializer or initializers.Ones()
-        self.scale_trainable = scale_trainable
+        self.accumulator_initializer = accumulator_initializer or initializers.Ones()
+        self.use_accumulators = use_accumulators
         self._distribution = self._num_scopes = self._num_decomps = None
 
     def _build_distribution(self, shape):
-        loc, scale = self._add_location_and_scale(shape)
-        return self._build_distribution_from_loc_and_scale(loc=loc, scale=scale)
+        if self.use_accumulators:
+            denom_accumulator, num_accumulator, scale = \
+                self._create_loc_accumulators_and_scale(shape)
+            loc = num_accumulator / denom_accumulator
+            dist = self._build_distribution_from_loc_and_scale(loc=loc, scale=scale)
+            return LocationScaleEMGradWrapper(dist, denom_accumulator, num_accumulator)
+        else:
+            loc, scale = self._create_location_and_scale_variable(shape)
+            return self._build_distribution_from_loc_and_scale(loc=loc, scale=scale)
+
+    def _create_loc_accumulators_and_scale(self, shape):
+        denom_accumulator = self.add_weight(
+            name="denom_accumulator", shape=shape, initializer=self.accumulator_initializer,
+            trainable=self.location_trainable)
+        num_accumulator = self.add_weight(
+            name="num_accumulator", shape=shape, initializer=self.location_initializer,
+            trainable=self.location_trainable)
+        scale = self.add_weight(
+            name="scale", shape=shape, initializer=self.scale_initializer, trainable=False)
+        return denom_accumulator, num_accumulator, scale
 
     @abc.abstractmethod
     def _build_distribution_from_loc_and_scale(self, loc, scale):
         """ Implement in descendant classes"""
 
-    def _add_location_and_scale(self, shape):
+    def _create_location_and_scale_variable(self, shape):
         loc = self.add_weight(
             name="location", shape=shape, initializer=self.location_initializer,
             trainable=self.location_trainable)
@@ -48,8 +69,9 @@ class LocationScaleLeafBase(BaseLeaf):
         config = dict(
             scale_initializer=initializers.serialize(self.scale_initializer),
             location_initializer=initializers.serialize(self.location_initializer),
-            scale_trainable=self.scale_trainable,
-            location_trainable=self.location_trainable
+            accumulator_intiailizer=initializers.serialize(self.accumulator_initializer),
+            use_accumulators=self.use_accumulators,
+            location_trainable=self.location_trainable,
         )
         base_config = super(LocationScaleLeafBase, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
--- libspn_keras/math/soft_em_grads.py
+++ libspn_keras/math/soft_em_grads.py
@@ -24,3 +24,38 @@ def log_softmax_from_accumulators_with_em_grad(accumulator, axis=None):
         return out, grad
 
     return _inner(accumulator)
+
+
+class LocationScaleEMGradWrapper:
+
+    def __init__(self, location_scale_distribution, accumulator, data_accumulator):
+        self.location_scale_distribution = location_scale_distribution
+        self.accumulator = accumulator
+        self.data_accumulator = data_accumulator
+
+    def __getattr__(self, attr):
+        # see if this object has attr
+        # NOTE do not use hasattr, it goes into
+        # infinite recursion
+        if attr in self.__dict__:
+            # this object has it
+            return getattr(self, attr)
+
+        # proxy to the wrapped object
+        return getattr(self.location_scale_distribution, attr)
+
+    def log_prob(self, x):
+
+        @tf.custom_gradient
+        def _inner(accumulator, data_accumulator):
+
+            def grad(dy):
+                return tf.reduce_sum(dy, axis=0, keepdims=True), \
+                       tf.reduce_sum(x * dy, axis=0, keepdims=True)
+
+            out = self.location_scale_distribution.log_prob(x)
+
+            return out, grad
+
+        return _inner(self.accumulator, self.data_accumulator)
+
